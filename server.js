dbce1dbe56f556fe414598a85d6aa980c1ed0487
amagineLost <124447978+amagineLost@users.noreply.github.com>
2024-09-08 23:15:33 -0500

    Update server.js

--- server.js
+++ server.js
@@ -19,21 +19,6 @@ app.get('/api/group-roles', async (req, res) => {
     }
 });
 
-// Endpoint to get members of a role
-app.get('/api/role-members/:roleId', async (req, res) => {
-    const groupId = '11592051'; // Replace with your actual group ID
-    const roleId = req.params.roleId;
-    const apiUrl = `https://api.roblox.com/groups/${groupId}/roles/${roleId}/members`;
-
-    try {
-        const response = await axios.get(apiUrl);
-        res.send(response.data);
-    } catch (error) {
-        console.error('Error fetching role members:', error);
-        res.status(500).send('Error fetching role members');
-    }
-});
-
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
